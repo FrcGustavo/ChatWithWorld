7f4f158319d01888446c56d9ecaf20b3298f7a2e
Francisco Gustavo <hidalgo_fco520@outlook.com>
2020-07-06 02:31:02 -0500

    👷‍♂️[Add] create connection to client to server

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,28 +1,31 @@
 import React, { useEffect, useState } from 'react';
 import io from 'socket.io-client';
 
-const Chat = ({ history }) => {
+let socket;
+
+const Chat = () => {
     const [name, setName] = useState('');
     const [room, setRoom] = useState('');
-    const socket = io('http://localhost:5000');
-
+    
     useEffect(() => { 
-        const query = history.location.search.split(/[&?=]/);
+        const query = window.location.search.split(/[&?=]/);
+        const name = query[2];
+        const room = query[4];
         
-        setName(query[2]);
-        setRoom(query[4]);
+        socket = io('http://localhost:5000');
 
+        setName(name);
+        setRoom(room);
+        
         socket.emit('join', { name, room }, (error) => {
             if (error) {
                 console.log('MI ERROR', error);
             }
         });
-
-
-    }, [history.location.search]);
+    }, [window.location.search]);
 
     return (
-    <h1>Nombre: {name} Sala: {room}</h1>
+        <h1>Nombre: {name} Sala: {room}</h1>
     );
 };
 
--- server/index.js
+++ server/index.js
@@ -10,4 +10,16 @@ app.get('/', (req, res) => {
     res.send('Hello this proyect you can see in http://localhost:3000');
 });
 
+io.on('connect', (socket) => {
+    console.log('Conectado');
+    socket.on('join', ({ name, room }, callback) => {
+        console.log('DATA', name, room);
+        callback()
+    });
+
+    socket.on('disconnect', () => {
+        console.log('BYE');
+    });
+});
+
 server.listen(process.env.PORT || 5000, () => console.log('server is running'));
