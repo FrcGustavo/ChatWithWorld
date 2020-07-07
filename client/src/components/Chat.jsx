00b887f6abc0912889f2aeecda474359183030b1
Francisco Gustavo <hidalgo_fco520@outlook.com>
2020-07-07 02:55:59 -0500

    👷‍♂️[Add] Chat without styles

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -6,7 +6,10 @@ let socket;
 const Chat = () => {
     const [name, setName] = useState('');
     const [room, setRoom] = useState('');
-    
+    const [users, setUsers] = useState('');
+    const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState([]);
+
     useEffect(() => { 
         const query = window.location.search.split(/[&?=]/);
         const name = query[2];
@@ -24,8 +27,45 @@ const Chat = () => {
         });
     }, [window.location.search]);
 
+    useEffect(() => {
+        socket.on('message', (message) => {
+            setMessages((messages) => [ ...messages, message ]);
+            console.log('NUEVO MENSAJE');
+        });
+
+        socket.on('roomData', ({ room, users }) => {
+            console.log(room, users);
+            setUsers(users);
+        });
+    }, []);
+
+    const handleChange = (event) => {
+        console.log(message);
+        setMessage(event.target.value);
+    };
+
+    const sendMessage = (event) => {
+            event.preventDefault();
+
+            if (message) {
+                socket.emit('sendMessage', message, () => setMessage(''));
+                console.log('Enviando');
+                
+            }
+    };
+
     return (
-        <h1>Nombre: {name} Sala: {room}</h1>
+        <div>
+            <h1>Nombre: {name} Sala: {room}</h1>
+            {
+                messages.map(m => <p key={m.text}>{m.text}</p>)
+            }
+            {
+                users === '' ? null : users.map(m => <p key={m.name}>{m.name}</p>)
+            }
+            <input type="text" onChange={handleChange} value={message} />
+            <input type="button" value="Enviar" onClick={sendMessage} />
+        </div>
     );
 };
 
